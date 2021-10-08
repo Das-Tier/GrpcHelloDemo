447d7693a0e569cfefb7f7e1995820788ed3660c
vitalii.mohola <vitalijmogola@gmail.com>
2021-10-08 16:57:24 +0300

    init commit

---
+++ GrpcHelloWorldClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Net.Client;
+using GrpcHelloWorldClient.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcHelloWorldClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new HelloService.HelloServiceClient(channel);
+            var reply = await client.SayHelloAsync(
+                    new HelloRequest { Name = "Vitalii" }
+                );
+
+            Console.WriteLine("Greetings: " + reply.Message);
+            Console.ReadLine();
+        }
+    }
+}
